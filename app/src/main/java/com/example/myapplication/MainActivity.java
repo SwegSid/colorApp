336fb19adf68a746dc808c133bf228ffaaed8070
SwegSid <bhap2002@yahoo.com>
2022-09-09 10:33:23 -0500

    made a function

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    int count;
+    @SuppressLint("SetTextI18n")
+    public void click(View view){
+        count++;
+        TextView end = findViewById(R.id.textView3);
+        EditText name = findViewById(R.id.editTextTextPersonName);
+
+        end.setText(""+name + " clicked " + count +" times");
+    }
+//sid is cute:)
 }
